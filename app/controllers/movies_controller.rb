cd7f5eb369f4764c8f310371a9d71ec1e33de25e
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-02 10:17:31 -0800

    feat: user story 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,11 @@ class MoviesController < ApplicationController
     @actor_age = @movie.actors.average(:age).round(0).to_i
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    @actor = Actor.find(params[:actor_id])
+    ActorMovie.create(actor: @actor, movie: @movie)
+    redirect_to "/movies/#{@movie.id}"
+  end
+
 end
--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'movie show page' do
     @actor3 = @movie.actors.create!(name: "Will", age: 92)
     @actor2 = @movie.actors.create!(name: "Bob", age: 26)
     @actor4 = @movie.actors.create!(name: "Jane", age: 138)
+    @actor5 = Actor.create!(name: "Annie", age: 24)
   end
 
   it 'has the movies info and lists its actors from youngest to oldest and gives average age' do
@@ -20,8 +21,32 @@ RSpec.describe 'movie show page' do
       expect(@actor1.name).to appear_before(@actor2.name)
       expect(@actor2.name).to appear_before(@actor3.name)
       expect(@actor3.name).to appear_before(@actor4.name)
+      expect(page).to_not have_content(@actor5.name)
       expect(page).to have_content("Average Actor Age: 70")
     end
+  end
+
+  it 'has the ability to add another actor to a movie' do
+    visit "/movies/#{@movie.id}"
+
+    expect(page).to have_content("Add Actor")
+    expect(page).to have_field(:actor_id)
+    expect(page).to have_button("Submit")
+
+    fill_in :actor_id, with: "#{@actor5.id}"
+    
+    click_button("Submit")
+    save_and_open_page
+    expect(current_path).to eq("/movies/#{@movie.id}")
+
+    within '#actors' do
+      expect(@actor1.name).to appear_before(@actor5.name)
+      expect(@actor5.name).to appear_before(@actor3.name)
+      expect(page).to have_content("Average Actor Age: 61")
+
+    end
+
+
   end
 
 
