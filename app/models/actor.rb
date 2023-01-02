55e1c7f751e16bf9c388491d8e007231ab08aabb
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-02 08:37:19 -0800

    wrote test for story 1

---
+++ app/models/actor.rb
@@ -0,0 +1,5 @@
+class Actor < ApplicationRecord
+  has_many :actors_movies
+  has_many :movies, through: :actors_movies
+
+end
---
+++ db/migrate/20230102160719_create_actors_movies.rb
@@ -0,0 +1,5 @@
+class CreateActorsMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :actors, :movies
+  end
+end
